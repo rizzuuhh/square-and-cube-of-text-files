fcc6e287c32aa7b8f5bb993e1b832fe6286fe532
rizza <denostarizza43@gmail.com>
2023-04-27 22:56:59 +1200

    Pseudocode

---
+++ text_file3.py
@@ -0,0 +1,14 @@
+# Read the input file
+
+# Convert numbers to integers
+
+# Separate the even and odd numbers
+    
+# Calculate the squares of even numbers and cubes of odd numbers
+    
+# Create output files
+  
+# Print success message
+
+
+
