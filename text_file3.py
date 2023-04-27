0ff5e2594e231af32a795bde7a9c44ebc14f717b
rizza <denostarizza43@gmail.com>
2023-04-27 23:25:03 +1200

    calculate square of even and cube of odd numbers

--- text_file3.py
+++ text_file3.py
@@ -10,8 +10,10 @@ def create_output_files(input_file):
     even_numbers = [n for n in numbers if n % 2 == 0]
     odd_numbers = [n for n in numbers if n % 2 == 1]
     
-# Calculate the squares of even numbers and cubes of odd numbers
-    
+    # Calculate the squares of even numbers and cubes of odd numbers
+    even_squares = [n**2 for n in even_numbers]
+    odd_cubes = [n**3 for n in odd_numbers]
+   
 # Create output files
   
 # Print success message
